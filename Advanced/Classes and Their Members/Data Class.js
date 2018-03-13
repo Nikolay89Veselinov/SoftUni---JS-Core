258e5caa60e060fee6834a6d90c4b5d2437f01b3
Nikolay89Veselinov <nikolay89veselinov@gmail.com>
2018-03-13 12:32:02 +0200

    Exercises

---
+++ Advanced/Classes and Their Members/Data Class.js
@@ -0,0 +1,16 @@
+    class Request {
+        constructor(method, uri, version, messege,) {
+            this.method = method
+            this.uri = uri
+            this.version = version
+            this.message = messege
+            this.response = undefined
+            this.fulfilled = false
+        }
+
+    }
+    
+
+let myData = new Request('GET',
+'http://google.com', 'HTTP/1.1', '')
+console.log(myData)
